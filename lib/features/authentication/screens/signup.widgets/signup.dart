9e081b9e50b89a8f3cd18997744844a43d8921cb
aderemixn <aderemiadex7@gmail.com>
2026-06-01 15:36:45 +0100

    Describe changes

--- lib/features/authentication/screens/signup.widgets/signup.dart
+++ lib/features/authentication/screens/signup.widgets/signup.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_utils/src/extensions/string_extensions.dart';
+import 'package:t_store/common/widgets.login_signup/form_divider.dart';
+import 'package:t_store/common/widgets.login_signup/social_buttons.dart';
+import 'package:t_store/features/authentication/screens/signup.widgets/signup_form.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
 
@@ -7,16 +11,29 @@ class SignupScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+      return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsetsGeometry.all(TSizes.defaultSpace),
+          padding: const EdgeInsetsGeometry.all(TSizes.defaultSpace),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
             /// Title
             Text(TTexts.signupTitle, style: Theme.of(context).textTheme.headlineMedium),
-            
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+
+            /// Form
+            const TSignupForm(),
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            /// Divider
+            TFormDivider(dividerText: TTexts.orSignInWith.capitalize!),
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            /// Social Buttons
+            const TSocialButtons(),
             ]
           ),
           ),
@@ -24,3 +41,4 @@ class SignupScreen extends StatelessWidget {
     );
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/utils/theme/theme.dart';
-
 import 'app.dart';
+
 
 void main() {
 
