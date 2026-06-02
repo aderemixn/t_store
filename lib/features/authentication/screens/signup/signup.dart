0f714fa88cebff576a74ba554b72573871aebaee
aderemixn <aderemiadex7@gmail.com>
2026-06-02 03:19:09 +0100

    Describe changes

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_utils/src/extensions/string_extensions.dart';
-import 'package:t_store/common/widgets.login_signup/form_divider.dart';
-import 'package:t_store/common/widgets.login_signup/social_buttons.dart';
-import 'package:t_store/features/authentication/screens/signup.widgets/signup_form.dart';
+import 'package:t_store/common/widgets/login_signup/form_divider.dart';
+import 'package:t_store/common/widgets/login_signup/social_buttons.dart';
+import 'package:t_store/features/authentication/screens/signup/widgets/signup_form.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
 
--- lib/features/authentication/screens/signup/widgets/signup_form.dart
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:t_store/features/authentication/screens/signup.widgets/term_conditions_checkbox.dart';
+import 'package:t_store/features/authentication/screens/signup/verify_email.dart';
+import 'package:t_store/features/authentication/screens/signup/widgets/term_conditions_checkbox.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
 
@@ -63,7 +65,13 @@ class TSignupForm extends StatelessWidget {
         const TTermsAndConditionCheckbox(),
         const SizedBox(height: TSizes.spaceBtwSections),
         /// Sign Up Button
-        SizedBox(width: double.infinity, child: ElevatedButton(onPressed: (){}, child: const Text(TTexts.createAccount)),)
+        SizedBox(
+          width: double.infinity, 
+          child: ElevatedButton(
+            onPressed: () => Get.to(const VerifyEmailScreen()), 
+            child: const Text(TTexts.createAccount)
+            ),
+            )
     
       ],
     )
